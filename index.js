2c2d99fbac7431c3e238e819f7e064384b8ffd21
Jordy Pouw <jordy@stamkracht.com>
2018-01-15 14:57:52 +0100

    change package main, add index with required plugins

---
+++ index.js
@@ -0,0 +1,4 @@
+require('./dist/trumbowyg.js');
+require('./dist/plugins/upload/trumbowyg.upload.js');
+require('./dist/plugins/uploadvideo/trumbowyg.uploadvideo.js');
+require('./dist/plugins/colors/trumbowyg.colors.js');
